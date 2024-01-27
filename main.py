9fabe683a1788dfcb87f64012e82175806aea917
Aryan Chawla <aryanchawlad@gmail.com>
2024-01-27 00:13:41 -0500

    main-menu

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 
 # pygame setup
 pygame.init()
+<<<<<<< HEAD
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 #
@@ -10,6 +11,34 @@ running = True
 dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+=======
+screen = pygame.display.set_mode((1193, 498))
+clock = pygame.time.Clock()
+running = True
+bg = pygame.image.load("background.jpg")
+pygame.font.init()
+print(pygame.font.get_fonts())
+my_font = pygame.font.SysFont('oldenglishtext', 30)
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+icon_1 = pygame.Vector2(335, 130)
+icon_2 = pygame.Vector2(834, 167)
+icon_3 = pygame.Vector2(465, 62)
+icon_4 = pygame.Vector2(728, 95)
+
+offset = pygame.Vector2(-55, 110)
+# create a surface object, image is drawn on it.
+imp = pygame.image.load("pin.png")
+imp1 = pygame.transform.scale(imp, (85, 85))
+imp2 = pygame.transform.scale(imp, (85, 85))
+imp3 = pygame.transform.scale(imp, (85, 85))
+imp4 = pygame.transform.scale(imp, (85, 85))
+text_surface_1 = my_font.render('Stage 1: Combine', False, (0,0,0), (255,255,255))
+text_surface_2 = my_font.render('Stage 2: Ferment', False, (0,0,0), (255,255,255))
+text_surface_3 = my_font.render('Stage 3: Dilute', False, (0,0,0), (255,255,255))
+text_surface_4 = my_font.render('Stage 4: Bottle', False, (0,0,0), (255,255,255))
+
+>>>>>>> aa6e90d (main-menu)
 
 while running:
     # poll for events
@@ -18,6 +47,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+<<<<<<< HEAD
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
@@ -33,12 +63,43 @@ while running:
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
 
+=======
+>>>>>>> aa6e90d (main-menu)
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
+<<<<<<< HEAD
     dt = clock.tick(60) / 1000
+=======
+    dt = clock.tick(60)
+    screen.blit(bg, (0, 0))
+    rec1 = screen.blit(imp1, icon_1)
+    rec2 = screen.blit(imp2, icon_2)
+    rec3 = screen.blit(imp3, icon_3)
+    rec4 = screen.blit(imp4, icon_4)
+    if rec1.collidepoint(pygame.mouse.get_pos()):
+        imp1 = pygame.transform.scale(imp, (100,100))
+        screen.blit(text_surface_1, icon_1 + offset)
+    else:
+        imp1 = pygame.transform.scale(imp, (85, 85))
+    if rec2.collidepoint(pygame.mouse.get_pos()):
+        imp2 = pygame.transform.scale(imp, (100,100))
+        screen.blit(text_surface_2, icon_2 + offset)
+    else:
+        imp2 = pygame.transform.scale(imp, (85, 85))
+    if rec3.collidepoint(pygame.mouse.get_pos()):
+        imp3 = pygame.transform.scale(imp, (100,100))
+        screen.blit(text_surface_3, icon_3 + offset)
+    else:
+        imp3 = pygame.transform.scale(imp, (85, 85))
+    if rec4.collidepoint(pygame.mouse.get_pos()):
+        imp4 = pygame.transform.scale(imp, (100,100))
+        screen.blit(text_surface_4, icon_4 + offset)
+    else:
+        imp4 = pygame.transform.scale(imp, (85, 85))
+>>>>>>> aa6e90d (main-menu)
 
 pygame.quit()
